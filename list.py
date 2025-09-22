65be5e56c02d6ea64cd818c8a3cbd7718631f891
aashirraja156-svg <aashirraja156@gmail.com>
2025-09-22 14:58:59 +0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,4 @@
+fruits = ["apple", "banana", "cherry"]
+fruits.append("mango")
+print(fruits[0])   # apple
+print(fruits)      # ['apple', 'banana', 'cherry', 'mango']
---
+++ variables.py
@@ -0,0 +1,8 @@
+name = "Aashir"
+age = 20
+pi = 3.14
+
+print("Name:", name)
+print("Age:", age)
+print("PI Value:", pi)
+
